fca7fca2160e28e237b182d1a008b3539154adf7
Insha Mahfooz <insha8471@gmail.com>
2025-07-01 17:57:31 +0530

    Hamburger updated

--- src/components/UI/Headers.jsx
+++ src/components/UI/Headers.jsx
@@ -1,6 +1,14 @@
+import { useState } from "react"
+import { GiHamburgerMenu } from "react-icons/gi"
 import { NavLink } from "react-router-dom"
 
 export const Headers = () => {
+
+    const [showMenu, setShowMenu] = useState(false);
+    const handleButtonToggle = () => {
+        return setShowMenu(!showMenu);
+    }
+
     return <header>
         <div className="container">
                 <div className="grid navbar-grid">
@@ -10,7 +18,7 @@ export const Headers = () => {
                         </NavLink>
                     </div>
                 
-                <nav>
+                <nav className={showMenu ? "menu-mobile" : "menu-web"}>
                     <ul>
                         <li><NavLink to={'/'}>Home</NavLink></li>
                         <li><NavLink to={'about'}>About</NavLink></li>
@@ -18,6 +26,12 @@ export const Headers = () => {
                         <li><NavLink to={'contact'}>Contact</NavLink></li>
                     </ul>
                 </nav>
+
+                <div className="ham-menu">
+                    <button onClick={handleButtonToggle}>
+                        <GiHamburgerMenu />
+                    </button>
+                </div>
             </div>
         </div>
     </header>
