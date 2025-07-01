816ecece60b2796141897ef2f119321175454023
Insha Mahfooz <insha8471@gmail.com>
2025-07-01 17:23:59 +0530

    footer changes updated

--- src/components/UI/Footers.jsx
+++ src/components/UI/Footers.jsx
@@ -57,7 +57,7 @@ export const Footers = () => {
                 </li>
                 <li>
                   <NavLink
-                    to="https://thapatechnical.shop/source-code"
+                    to="https://github.com/insha8471/worldAtlas.git"
                     target="_blank"
                   >
                     Source Code
